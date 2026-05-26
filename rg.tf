18e10f7df48998a22f80d200b7b06125b91bb1c2
dhwajaa5-ui <dhwajaa5@gmail.com>
2026-05-26 23:50:12 +0530

    Add new rg in rg.tf
    Adding new RG as per Request Number - RITM243545

--- rg.tf
+++ rg.tf
@@ -3,3 +3,10 @@ resource "azurerm_resource_group" "rg01"
   name = "dhwaja_rg01"
   location="central india"
 }
+
+resource "azurerm_resource_group" "rg02"
+{
+  name = "dhwaja_rg02"
+  location="central us"
+}
+
